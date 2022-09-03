f37b5646ee774ba94c6719f7e03f927a2376d5af
Kunwoo Ahn <dryrain@fastmail.com>
2022-09-03 18:26:27 +0900

    버전 정보 수정

--- route/misc/version.py
+++ route/misc/version.py
@@ -1,34 +1,13 @@
-import base64
-import copy
-import json
 import os
-import re
-import secrets
-import shutil
 
-import bcrypt
-import diskcache
-from fastapi import APIRouter, HTTPException
-from sentry_sdk import start_transaction, start_span
-from sqlitedict import SqliteDict
-from starlette.requests import Request
-from starlette.responses import Response
-from starlette.templating import Jinja2Templates
-
-from VO.account_vo import ChangePasswordAction, AccountAction
-from config.config import USER_DB_PATH
-from fluent_logger.PromoFormat import PromoLogger, ActionCode
-from route.user.account import validate
-from service.login import login
-from service.register import register
-import logging
+from fastapi import APIRouter
 
 router = APIRouter()
 
 
 @router.get("/")
 async def get_version():
-    version = "2022090301"
+    version = "DOCKER_2022090318"
     try:
         node = os.uname().nodename
     except:
@@ -38,10 +17,8 @@ async def get_version():
     if node_name:
         node = node_name
 
-    project_name = "attend.miscthings.net"
-
     return {
         "version": version,
         "node": node,
-        "version_string": f"{node}.{version}.{project_name}"
+        "version_string": f"서버: {node} · 버전: {version}"
     }
