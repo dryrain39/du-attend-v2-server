1bab35c3aea1c7e44a65476e76351999d0e4e475
Kunwoo Ahn <dryrain@fastmail.com>
2022-10-10 02:29:10 +0900

    search 관련 가오픈

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug", debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True, log_level="debug", debug=True)
