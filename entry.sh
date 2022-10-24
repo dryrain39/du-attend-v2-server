b4a5dc8d789fbd7770f8564848216fbb39a0c8a3
Kunwoo Ahn <dryrain@fastmail.com>
2022-10-25 00:57:08 +0900

    proxy header 받게 변경

--- entry.sh
+++ entry.sh
@@ -1,3 +1,3 @@
 mkdir $HOME/.postgresql/ ; cp ./config/root.crt $HOME/.postgresql/
 
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers
